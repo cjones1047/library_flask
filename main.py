8e442d43a9727f489ffa4601ea52f5506dcdbe38
cjones1047 <cjmj1047@gmail.com>
2023-02-16 06:17:11 -0600

    FlaskForm and Bootstrap classes imported along with multiple wtforms with validators to construct field to add new book, rendered and processed in add template; add() currently validates form on submission; add() prints values user entered into forms

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, IntegerField
+from wtforms.validators import DataRequired, Email, Length, NumberRange
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+app.secret_key = 'kldjfe98ujsfdlf098ejfds0f9dsjfj'
+Bootstrap(app)
 
 all_books = []
+
+
+class BookForm(FlaskForm):
+    title = StringField('Book Name', validators=[DataRequired()])
+    author = StringField('Book Author', validators=[DataRequired()])
+    rating = IntegerField('Rating', validators=[DataRequired(), NumberRange(min=1, max=5)])
+    submit = SubmitField('Add Book')
 
 
 @app.route('/')
@@ -10,9 +23,15 @@ def home():
     return render_template('index.html')
 
 
-@app.route("/add")
+@app.route("/add", methods=["GET", "POST"])
 def add():
-    return render_template('add.html')
+    form = BookForm()
+    if form.validate_on_submit():
+        print(form.title.data)
+        print(form.author.data)
+        print(form.rating.data)
+
+    return render_template('add.html', form=form)
 
 
 if __name__ == "__main__":
